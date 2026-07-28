0309cd40eabcc9f6cf60faf6b16675e969a774d8
Henrik Strocka <henstr@hotmail.com>
2026-07-28 20:29:31 +0200

    Fixed devices mounting from outside shell

--- lib/shells/devshell/entrypoint.nix
+++ lib/shells/devshell/entrypoint.nix
@@ -1,5 +1,9 @@
-{ nativePkgs, cfg, networkCmds, inner }:
-
+{
+  nativePkgs,
+  cfg,
+  networkCmds,
+  inner,
+}:
 nativePkgs.writeShellScript "${cfg.board.name}-entrypoint" ''
   export PATH="${nativePkgs.iproute2}/bin:${nativePkgs.util-linux}/bin:${nativePkgs.coreutils}/bin:${nativePkgs.busybox}/bin:$PATH"
 
@@ -12,8 +16,21 @@ nativePkgs.writeShellScript "${cfg.board.name}-entrypoint" ''
 
   mount -t proc proc "$ROOTFS/proc"
 
-  mount --rbind /dev "$ROOTFS/dev"
-  mount --make-rslave "$ROOTFS/dev"
+  mount -t tmpfs devtmpfs "$ROOTFS/dev" -o mode=0755,size=1m
+
+  for _dev in null zero full random urandom tty; do
+    [ -e "/dev/$_dev" ] && touch "$ROOTFS/dev/$_dev" && mount --bind "/dev/$_dev" "$ROOTFS/dev/$_dev" || true
+  done
+
+  ln -sf /proc/self/fd   "$ROOTFS/dev/fd"
+  ln -sf /proc/self/fd/0 "$ROOTFS/dev/stdin"
+  ln -sf /proc/self/fd/1 "$ROOTFS/dev/stdout"
+  ln -sf /proc/self/fd/2 "$ROOTFS/dev/stderr"
+
+  mkdir -p "$ROOTFS/dev/pts"
+  mount -t devpts devpts "$ROOTFS/dev/pts" -o newinstance,ptmxmode=0666,mode=0620
+
+  ln -sf pts/ptmx "$ROOTFS/dev/ptmx"
 
   mkdir -p "$ROOTFS/nix/store"
   mount --bind /nix/store "$ROOTFS/nix/store"
